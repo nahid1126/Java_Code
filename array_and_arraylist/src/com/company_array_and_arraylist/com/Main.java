9e722b40c1f910c7754810a5ed6ccf0fe40b0710
Nahid Hasan <45915292+nahid1126@users.noreply.github.com>
2020-01-28 11:23:22 +0600

    Update Main.java

--- array_and_arraylist/src/com/company_array_and_arraylist/com/Main.java
+++ array_and_arraylist/src/com/company_array_and_arraylist/com/Main.java
@@ -1,10 +1,6 @@
 package com.company_array_and_arraylist.com;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
-import java.util.Iterator;
+import java.util.*;
 
 public class Main {
 
@@ -13,20 +9,29 @@ public class Main {
         Scanner input=new Scanner(System.in);
         System.out.println("Enter the array size : ");
         int n=input.nextInt();
-        int [] array=new int[n];
+        Integer [] array=new Integer[n];
         System.out.println("Enter the array elements : ");
         for (int i=0;i<n;i++){
             array[i]=input.nextInt();
         }
 
-        //array sort
+        //array sort ascending order
         Arrays.sort(array);
+
         //for each loop
         for (int x:array){
             System.out.print(x +" ");
         }
         System.out.println();
+        //Array sort Descending order
+        Arrays.sort(array,Collections.reverseOrder());
+        for (int i=0;i<array.length;i++)
+        {
+            System.out.print(array[i]+" ");
+        }
+        System.out.println();
         //ArrayList
+        System.out.println("Enter the arrayList data :");
         ArrayList<Integer>data=new ArrayList<Integer>();
         for (int i=0;i<n;i++){
         data.add(input.nextInt());
@@ -34,9 +39,11 @@ public class Main {
 
         //Iterator in arrylist
         Iterator itr=data.iterator();
+
         while (itr.hasNext()){
-            System.out.println(itr.next()+" ");
+            System.out.print(itr.next()+" ");
         }
+        System.out.println();
         //arraylist sort
         Collections.sort(data);
 
@@ -44,7 +51,13 @@ public class Main {
         for (int d:data){
             System.out.print(d+" ");
         }
+        System.out.println();
 
+        //ArrayList sort descending order
+        Collections.sort(data,Collections.reverseOrder());
+        for (int a:data) {
+            System.out.print(a+" ");
+        }
 
     }
 }
