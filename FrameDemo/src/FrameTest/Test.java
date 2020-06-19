c0f21523567b321842ea19277689f3ef9e3e039e
nahid <nahidnub1126@gmail.com>
2020-06-20 00:44:14 +0600

    working with jframe and create jframe multiple way

--- FrameDemo/src/FrameTest/Test.java
+++ FrameDemo/src/FrameTest/Test.java
@@ -3,12 +3,23 @@ package FrameTest;
 import javax.swing.*;
 import java.awt.*;
 
+import static java.awt.Color.green;
+
 public class Test extends JFrame {
+    private ImageIcon ic = new ImageIcon(getClass().getResource("hi.png"));
+    private Container C;
     Test(){
+        anotherfun();
+    }
+    public void anotherfun(){
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(700,400,500,300);
         setTitle("New Frame");
-        setBackground(Color.green);
+
+        this.setIconImage(ic.getImage());
+
+        C=this.getContentPane();
+        C.setBackground(green);
         setResizable(false);
     }
     public static void main(String[] args) {
